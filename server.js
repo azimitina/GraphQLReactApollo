b6d84e02f28b735805581a5465a8239b11329251
azimitina <azimitina@yahoo.com>
2019-11-07 23:16:25 +1100

    Created - LaunchType / RocketType Schema

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const schema = require('./schema');
 
 const app = express();
 
-app.use('/graphql',
-graphqlHTTP({
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
-    graphiql: true,
-  }),
+    graphiql: true
+  })
 );
 
 const PORT = process.env.PORT || 5000;
